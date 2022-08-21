66a4c56f9680ad80216649075f24179e10ec9277
chandrasekhar jetty <chandrasekhar.jetty@viavisolutions.com>
2022-08-21 20:42:20 +0100

    removed credentials parameter in provider block

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -2,7 +2,7 @@ provider "google" {
   project = var.gcp_project
   region  = var.region
   zone    = var.zone
-  credentials = file("C:\\Users\\JET73937\\.ssh\\du-poc-aa47ede6945f.json")
+  #credentials = file("C:\\Users\\JET73937\\.ssh\\du-poc-aa47ede6945f.json")
 }
 
 module "Du_cluster" {
