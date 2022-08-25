d581197ebea14055324715516b1d82d23bd708a8
chandrasekhar jetty <chandrasekhar.jetty@viavisolutions.com>
2022-08-25 23:37:41 +0100

    corrected ECHO upper case to echo lower case

--- kubernetes_cluster/replay.sh
+++ kubernetes_cluster/replay.sh
@@ -14,7 +14,8 @@ fi
 
 echo "Started to push packets to destination"
 tcpreplay-edit -i eth0 --loop=$loop_count --enet-dmac=$dest_mac,$dest_mac  $input_file
-ECHO "Now starting the packet transfer from RU server"
-curl -X POST http://ru-service/action/ -H 'Content-Type: application/json' -d "{'loop': 5, 'dest_mac_address': $local_mac, 'action': "start"}"
+echo "Now starting the packet transfer from RU server"
+#curl -X POST http://ru-service/action/ -H 'Content-Type: application/json' -d "{'loop': 5, 'dest_mac_address': $local_mac, 'action': "start"}"
+curl -X POST http://du-service/action/ -H 'Content-Type: application/json' -d '{"loop": 5, "action": "start"}'
 #tcpreplay-edit --loop=5 -i $interface --enet-dmac=02:42:16:72:81:dd,02:42:16:72:81:dd /app/data/port0_2022_7_1_8_29_51_0.pcap
 #tcpreplay-edit --loop=2 -i enp0s3 --enet-dmac=02:42:16:72:81:dd,02:42:16:72:81:dd /app/data/port0_2022_7_1_8_29_51_0.pcap
