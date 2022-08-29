521c9783ae7b8713fb2f02898ab3fd6c39c6c00c
chandrasekhar jetty <chandrasekhar.jetty@viavisolutions.com>
2022-08-29 11:24:45 +0100

    Removed comments and add quotes to variables in if clause

--- kubernetes_cluster/start_pcap_process.sh
+++ kubernetes_cluster/start_pcap_process.sh
@@ -3,7 +3,9 @@ loop_count=$1
 interface="eth0"
 pod_role=$2
 
-if [ $pod_role == 'ru' ]
+echo "Given parameters are $loop_count, $interface, $pod_role"
+
+if [ "$pod_role" = "ru" ]
 then
   dest_mac=`curl -X GET http://du-service/mac/`
   input_file="ru_captured.pcap"
@@ -16,16 +18,12 @@ fi
 echo "Started to push packets to destination"
 tcpreplay-edit -i eth0 --loop=$loop_count --enet-dmac=$dest_mac,$dest_mac  $input_file
 
-if [ $pod_role != 'ru'  ] 
+if [ "$pod_role" != "ru"  ] 
 then
   echo "Stopping tcpdump capture on the RU pod"
   curl -X POST http://ru-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "stop"}'
   echo "Starting tcpdump capture on DU pod"
   curl -X POST http://du-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "start"}'
   echo "Now starting the packet transfer from RU server"
-  #echo "curl -X POST http://ru-service/action/ -H 'Content-Type: application/json' -d '{"loop": $loop_count, "pod_role": "ru", "action": "start"}'"
   curl -X POST http://ru-service/action/ -H 'Content-Type: application/json' -d '{"loop": 5, "pod_role": "ru", "action": "start"}'
 fi
-#curl -X POST http://du-service/action/ -H 'Content-Type: application/json' -d '{"loop": 5, "action": "start"}'
-#tcpreplay-edit --loop=5 -i $interface --enet-dmac=02:42:16:72:81:dd,02:42:16:72:81:dd /app/data/port0_2022_7_1_8_29_51_0.pcap
-#tcpreplay-edit --loop=2 -i enp0s3 --enet-dmac=02:42:16:72:81:dd,02:42:16:72:81:dd /app/data/port0_2022_7_1_8_29_51_0.pcap
