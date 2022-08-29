28523965ecd9cd0d81826ff0d0ee1a0370881cf0
chandrasekhar jetty <chandrasekhar.jetty@viavisolutions.com>
2022-08-29 16:22:12 +0100

    print command updated

--- kubernetes_cluster/main.py
+++ kubernetes_cluster/main.py
@@ -52,10 +52,10 @@ def get_mac_address():
 def pcap_trigger(input: CaptureInput):
     if input.action == "start":
         print("starting TCP DUMP capture..")
-        command = ''.join(["nohup", " ", "/home/api_scripts/tcp_capture.sh", " ", ">>", " ", "./out", "2>&1", "<&-", " ", "&"])
+        command = ''.join(["nohup ", "/home/api_scripts/tcp_capture.sh ", ">> ", "./out ", "2>& ",  "1<&- ", "&"])
         print("command is %s" %command )
-        p2 = subprocess.Popen(''.join(["nohup", " ", "/home/api_scripts/tcp_capture.sh ", ">> ", "./out ", "2>& ",  "1<&- ", "&"]), 
-                              shell=True, close_fds=True)
+        p2 = subprocess.Popen(''.join(["nohup ", "/home/api_scripts/tcp_capture.sh ", ">> ", "./out ", "2>& ",  "1<&- ", "&"]), 
+                              shell=True)
         print(p2.communicate())
         print("Process ID is %s" %p2.pid)
         return f"Started TCP DUMP capture in pod {os.getenv('HOSTNAME', None)}"
