aac9451a6c26e42bbc7c72e3307badd270b2c5b8
chandrasekhar jetty <chandrasekhar.jetty@viavisolutions.com>
2022-09-01 12:57:44 +0100

    tcpdump changes w.r.t side car container

--- kubernetes_cluster/start_pcap_process.sh
+++ kubernetes_cluster/start_pcap_process.sh
@@ -5,28 +5,24 @@ pod_role=$2
 
 echo "Given parameters are $loop_count, $interface, $pod_role"
 
-if [ "$pod_role" = "ru" ]
+if [ "$pod_role" = "du"  ] 
 then
-  dest_mac=`curl -X GET http://du-service/mac/`
-  input_file="/home/api_scripts/ru_captured.pcap"
-else
   dest_mac=`curl -X GET http://ru-service/mac/`
   input_file="port0_2022_7_1_8_29_51_0.pcap"
-  curl -X POST http://ru-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "start"}'
-fi
-
-echo "Started to push packets to destination"
-tcpreplay-edit -i eth0 --loop=$loop_count --enet-dmac=$dest_mac,$dest_mac  $input_file
-
-if [ "$pod_role" != "ru"  ] 
-then
-  echo "Stopping tcpdump capture on the RU pod"
-  curl -X POST http://ru-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "stop"}'
-  echo "Starting tcpdump capture on DU pod"
-  curl -X POST http://du-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "start"}'
+  echo "Started to push packets from DU to RU"
+  tcpreplay-edit -i eth0 --loop=$loop_count --enet-dmac=$dest_mac,$dest_mac  $input_file
+  #echo "Stopping tcpdump capture on the RU pod"
+  #curl -X POST http://ru-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "stop"}'
+  #echo "Starting tcpdump capture on DU pod"
+  #curl -X POST http://du-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "start"}'
   echo "Now starting the packet transfer from RU server"
   curl -X POST http://ru-service/action/ -H 'Content-Type: application/json' -d '{"loop": 5, "pod_role": "ru", "action": "start"}'
 else
-  echo "Stopping tcpdump capture on DU pod"
-  curl -X POST http://du-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "stop"}'
+  dest_mac=`curl -X GET http://du-service/mac/`
+  input_file="/pod_volume/ru_captured_copy.pcap"
+  echo "Started to push packets from RU to DU"
+  cp /pod_volume/ru_captured.pcap  /pod_volume/ru_captured_copy.pcap
+  tcpreplay-edit -i eth0 --loop=$loop_count --enet-dmac=$dest_mac,$dest_mac  $input_file
+  #echo "Stopping tcpdump capture on DU pod"
+  #curl -X POST http://du-service/tcp_capture/ -H 'Content-Type: application/json' -d '{"action": "stop"}'
 fi
